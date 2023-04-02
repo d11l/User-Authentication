1c0a58ac7768a7b4c4f37612980607934943cbef
Abdulrhman radhi <abdulrahman.radhi.2002@gmail.com>
2023-04-02 06:26:26 +0300

    I

---
+++ Routes/Routes.js
@@ -0,0 +1,60 @@
+const Router  = require('express').Router();
+const userModel = require('../Models/user');
+const bcrypt = require('bcrypt');
+
+
+const isAuth = (req, res, next) => req.session.isAuth ? next() : res.redirect('/login');
+;    
+    
+Router.get('/login'    , (req, res) => res.render("login"))   
+Router.get('/register' , (req, res) => res.render("register"));
+Router.get('/home'     , isAuth, (req, res) => res.render("home"));
+Router.get('/'         , (req, res) => res.redirect("/home"));
+
+Router.get('/logout', (req, res) => {
+    
+    req.session.destroy(err => {
+        if (err) throw err;
+        res.redirect("/login");
+    })
+    
+});
+    
+
+Router.post('/register' , async (req, res) =>{
+       
+const {username, email, password} = req.body ;
+    
+    let user = await userModel.findOne({email})
+    if(user) return res.redirect('/register');
+     
+    const hashPassword = await bcrypt.hash(password, 12);
+    
+    user = new userModel({
+    username,
+    email,
+    password: hashPassword
+    }); 
+    
+    await user.save();
+    
+    res.redirect("/login");
+});
+    
+    
+Router.post('/login' ,async (req, res) => {
+    
+    const {username , password} = req.body ;
+    let user = await userModel.findOne({username})
+    if(!user) return res.redirect('/login');
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    
+    if(!isMatch) return res.redirect('/login');
+        
+    req.session.isAuth = true;
+    res.redirect("/home")
+})
+        
+
+module.exports = Router;
---
+++ app.js
@@ -0,0 +1,35 @@
+const express  = require('express');
+const session  = require('express-session');
+const DBsession = require('connect-mongodb-session')(session)
+const mongoose = require('mongoose');
+const Router = require('./Routes/Routes');
+
+const app	= express();
+
+const DBuri = "mongodb://127.0.0.1:27017/App";// Ex: mongodb://127.0.0.1:27017/App
+
+mongoose.connect(DBuri,{
+    useNewUrlParser: true,
+    useCreateIndex: true ,
+    useUnifiedTopology: true
+}).then(() => console.log("Mongodb Connected"))
+
+
+const store = new DBsession({uri:DBuri , collection: "sessions"}) 
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store ,//
+    cookie:{maxAge:  1000 * 60 * 60 * 24} // 24h 
+}));
+
+app.use(Router);
+
+
+app.listen(3000, () => console.log("Listening on port 3000"));
